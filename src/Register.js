0b2f1305efe4de647608f87624e7adeea11b055c
LenaFWU <lena.f.wu@gmail.com>
2023-08-02 20:11:29 -0400

    initial commit

---
+++ src/Register.js
@@ -0,0 +1,4 @@
+import { useRef, useState, useEffect } from "react";
+import { faCheck, faTimes, faInfoCircle } from "@fortawesom
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
