4e2eed0ecfb67e552c3753b0650b189aecebde49
lenafwu <113751598+lenafwu@users.noreply.github.com>
2023-09-07 00:37:33 +0000

    updated cors

--- src/api/axios.js
+++ src/api/axios.js
@@ -3,13 +3,16 @@ import axios from "axios";
 // TODO: replace url
 export default axios.create({
   // baseURL: "https://used-product-app.lenafwu.repl.co",
-  baseURL: "http://localhost:3000",
-  // baseURL: "https://129.153.53.89",
+ // baseURL: "http://localhost:3000",
+//  baseURL: "https://129.153.53.89",
+ baseURL: "https://rou-ruan.com",
 });
 
 export const axiosPrivate = axios.create({
-  baseURL: "http://localhost:3000",
-  // baseURL: "https://129.153.53.89",
+ // baseURL: "http://localhost:3000",
+//  baseURL: "https://129.153.53.89",
+  baseURL: "https://rou-ruan.com",
+  
 
   headers: {
     "Content-Type": "application/json",
